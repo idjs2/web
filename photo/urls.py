c201111dec82a76178d8fb6590fb37694d86cab8
Jeongsik <idjs1102@naver.com>
2023-08-15 21:25:58 +0900

    재시작

--- photo/urls.py
+++ photo/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.photo_list, name='photo_list'),
-    path('', views.test, name='test'),
+    path('', views.photo_list, name='photo_list'),  # http://127.0.0.1:8000/
+    path('test', views.test, name='test'),  # http://127.0.0.1:8000/test
 ]
